316620271eba28ba54d3fd57b66dcad8ecb14afd
Pavel Kevlak <pauloosha@gmail.com>
2019-03-27 16:22:56 +0300

    Update task4.js

--- tasks/homework_5/task4.js
+++ tasks/homework_5/task4.js
@@ -1,15 +1,21 @@
 function showAuthorsNames() {
   const urls = [];
 
-  for(let index = 0; index < arguments.length; index++){
-    urls.push(
-      fetch(`https://jsonplaceholder.typicode.com/posts/${arguments[index]}`)
+  const getName = (id) => {
+    return (
+      fetch(`https://jsonplaceholder.typicode.com/posts/${arguments[id]}`)
       .then(response => response.json())
       .then(json => fetch((`https://jsonplaceholder.typicode.com/users/${json.userId}`)))
       .then(response => response.json())
       .then(json => json.name)
     )
   }
+
+  for(let index = 0; index < arguments.length; index++){
+    urls.push(getName(index));
+    console.log(urls);
+  }
+
   Promise.all( urls )
   .then(results => {
     const str = results.join(' ')
